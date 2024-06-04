d6eb4c8f9925097ffb6d9a504e4d4209a5162021
volcanic <1605090219@qq.com>
2024-06-04 10:10:55 +0800

    feat: coinGecko签到方式变更

--- check-in/coinGecko_daily_check_in.js
+++ check-in/coinGecko_daily_check_in.js
@@ -2,25 +2,26 @@ const coinGecko_daily_check_in = async () => {
   const { coingeckoCookie, coingeckoToken, coingeckoAuthToken } = process.env;
 
   try {
-    const res = await fetch(
-      "https://www.coingecko.com/account/candy/daily_check_in",
+    const authTokenRes = await fetch(
+      "https://www.coingecko.com/accounts/csrf_meta.json",
       {
-        method: "POST",
+        method: "GET",
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Accept: "*/*",
+          "Accept-Encoding": "gzip, deflate, br, zstd",
+          "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+          "Cache-Control": "no-cache",
           Cookie: coingeckoCookie,
-          Origin: "https://www.coingecko.com",
-          Referer: "https://www.coingecko.com/account/candy",
-          Accept:
-            "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+          Pragma: "no-cache",
+          Priority: "u=1, i",
+          Referer: "https://www.coingecko.com/en/candy",
           "Sec-Ch-Ua":
-            '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-
+            '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
           "Sec-Ch-Ua-Arch": "x86",
           "Sec-Ch-Ua-Bitness": "64",
-          "Sec-Ch-Ua-Full-Version": "123.0.6312.122",
+          "Sec-Ch-Ua-Full-Version": "125.0.6422.142",
           "Sec-Ch-Ua-Full-Version-List":
-            '"Google Chrome";v="123.0.6312.122", "Not:A-Brand";v="8.0.0.0", "Chromium";v="123.0.6312.122"',
+            '"Google Chrome";v="125.0.6422.142", "Chromium";v="125.0.6422.142", "Not.A/Brand";v="24.0.0.0"',
           "Sec-Ch-Ua-Mobile": "?0",
           "Sec-Ch-Ua-Model": "",
           "Sec-Ch-Ua-Platform": "Windows",
@@ -29,15 +30,43 @@ const coinGecko_daily_check_in = async () => {
           "Sec-Fetch-Mode": "cors",
           "Sec-Fetch-Site": "same-origin",
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-          "X-Csrf-Token": coingeckoToken,
-          "X-Requested-With": "XMLHttpRequest",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
         },
-        body: `authenticity_token=${coingeckoAuthToken}`,
+      }
+    ).then((response) => response.json());
+
+    const checkRes = await fetch(
+      "https://www.coingecko.com/en/candy/daily_check_in",
+      {
+        method: "POST",
+        headers: {
+          // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Cookie: coingeckoCookie,
+          Origin: "https://www.coingecko.com",
+          Referer: "https://www.coingecko.com/en/candy",
+          Accept: "*/*",
+          "Sec-Ch-Ua-Arch": "x86",
+          "Sec-Ch-Ua-Bitness": "64",
+          "Sec-Ch-Ua-Full-Version": "125.0.6422.142",
+          "Sec-Ch-Ua-Full-Version-List":
+            '"Google Chrome";v="125.0.6422.142", "Chromium";v="125.0.6422.142", "Not.A/Brand";v="24.0.0.0"',
+          "Sec-Ch-Ua-Mobile": "?0",
+          "Sec-Ch-Ua-Model": "",
+          "Sec-Ch-Ua-Platform": "Windows",
+          "Sec-Ch-Ua-Platform-Version": "10.0.0",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+          "X-Csrf-Token": authTokenRes.token,
+          // "X-Requested-With": "XMLHttpRequest",
+        },
+        // body: `authenticity_token=${coingeckoAuthToken}`,
       }
     ).then((response) => response.text());
 
-    console.log("coinGecko_daily_check_in", res);
+    console.log("coinGecko_daily_check_in", checkRes);
     return true;
   } catch (error) {
     console.log("error", error);
