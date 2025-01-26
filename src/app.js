e20e4441bf6f3b26984fed664f3b2cb8382a0465
volcanicll <1605090219@qq.com>
2025-01-27 01:46:09 +0800

    fix: love words and weather access

--- src/app.js
+++ src/app.js
@@ -3,24 +3,28 @@ const coinMarketService = require("./services/checkin/coinMarket");
 const wxBotService = require("./services/notification/wxBot/WxBotService");
 const groupBotService = require("./services/notification/groupBot/GroupBotService");
 const { getCurrentDayOfWeek } = require("./utils/date");
-const { getRandomGreeting, formatCheckInResult } = require("./utils/message");
+const { formatCheckInResult } = require("./utils/message");
+const { generateDailyMessage } = require("./utils/contentGenerator");
 
 async function run() {
   console.log("开始执行自动签到...");
 
   try {
     // 执行签到
-    const checkInResults = {
-      CoinGecko: await coinGeckoService.checkIn(),
-      CoinMarket: await coinMarketService.checkIn(),
-    };
+    // const checkInResults = {
+    //   CoinGecko: await coinGeckoService.checkIn(),
+    //   CoinMarket: await coinMarketService.checkIn(),
+    // };
 
     // 准备消息内容
-    const checkInResultText = formatCheckInResult(checkInResults);
+    // const checkInResultText = formatCheckInResult(checkInResults);
     const dayOfWeek = getCurrentDayOfWeek();
-    const greeting = getRandomGreeting();
 
-    const messageContent = `${checkInResultText}\n${dayOfWeek}了。爱你哦，${greeting}`;
+    // 获取天气和情话内容
+    const dailyMessage = await generateDailyMessage("北京");
+
+    // 组合所有内容
+    const messageContent = `${dailyMessage}\n\n${dayOfWeek}了`;
 
     // 发送通知
     const messageConfig = {
